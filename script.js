41cfe34c8eba57c569dddd9e85f1c1b563504491
IslamDevFull <it.front.back@gmail.com>
2022-06-13 19:36:08 +0200

    releas v1.1

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
 function xato(){
-    let xatolar = document.getElementById("xato").value;
-    let tugrilar = document.getElementById("tugri").value; 
-    if(xatolar != tugrilar){
-        document.getElementById("smsXato").style.visibility = "visible";
+    let xato = document.getElementById("xato").value;
+    let xato1 = document.getElementById("xato1").value;
+    let xato2 = document.getElementById("xato2").value;
+    let tugri = document.getElementById("tugri").value;
+    let tugri1 = document.getElementById("tugri1").value;
+    let tugri2 = document.getElementById("tugri2").value;
+    if(xato != tugri){
+        document.getElementById("smsXato").style.display = "block";
+        document.getElementById("qayta").style.visibility = "visible";
     }
-    if(xatolar == tugrilar){
-        document.getElementById("smsTugri").style.visibility = "visible";
+    if(xato == tugri){
+        document.getElementById("smsTugri").style.display = "block";
+        document.getElementById("qadam_2").style.display = "block";
     }
+}
+
+function none() {
+    document.getElementById('smsXato').style.display = "none";
 }
