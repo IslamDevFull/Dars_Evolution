42b8fd894215168253f6f2faa93a8e1d1cf52245
IslamDevFull <it.front.back@gmail.com>
2022-06-15 01:13:51 +0500

    Releas v1.2

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+/*function xato(){
+    let xato = document.getElementById("xato").value;
+    let xato1 = document.getElementById("xato1").value;
+    let xato2 = document.getElementById("xato2").value;
+    let tugri = document.getElementById("tugri").value;
+    let tugri1 = document.getElementById("tugri1").value;
+    let tugri2 = document.getElementById("tugri2").value;
+    if(xato != tugri & xato1 != tugri1){
+        document.getElementById("smsXato").style.display = "block";
+        document.getElementById("qayta").style.visibility = "visible";
+    }
+    if(xato == tugri & xato1 == tugri1){
+        document.getElementById("smsTugri").style.display = "block";
+        document.getElementById("qadam_2").style.display = "block";
+    }
+}*/
 function xato(){
     let xato = document.getElementById("xato").value;
     let xato1 = document.getElementById("xato1").value;
@@ -5,11 +21,11 @@ function xato(){
     let tugri = document.getElementById("tugri").value;
     let tugri1 = document.getElementById("tugri1").value;
     let tugri2 = document.getElementById("tugri2").value;
-    if(xato != tugri){
+    if(xato != tugri & xato1 != tugri1){
         document.getElementById("smsXato").style.display = "block";
         document.getElementById("qayta").style.visibility = "visible";
     }
-    if(xato == tugri){
+    if(xato == tugri & xato1 == tugri1){
         document.getElementById("smsTugri").style.display = "block";
         document.getElementById("qadam_2").style.display = "block";
     }
