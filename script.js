751ee25fa0b219eb1f030b1cc5a0c2c9e32d0f73
IslamDevFull <it.front.back@gmail.com>
2022-06-22 01:53:40 +0500

    mavzu bir qarib tax bo'ldi

--- script.js
+++ script.js
@@ -20,3 +20,49 @@ function xato(){
 function none() {
     document.getElementById('smsXato').style.display = "none";
 }
+
+function btn(){
+    document.getElementById('dis').style.display = 'block'
+    document.getElementById('btn1').style.display = 'block'
+}
+function btn1(){
+    document.getElementById('dis1').style.display = 'block'
+    document.getElementById('btn2').style.display = 'block'
+}
+function btn2(){
+    document.getElementById('dis2').style.display = 'block'
+    document.getElementById('btn3').style.display = 'block'
+}
+function btn3(){
+    document.getElementById('dis3').style.display = 'block'
+    document.getElementById('btn4').style.display = 'block'
+}
+function btn4(){
+    document.getElementById('dis4').style.display = 'block'
+    document.getElementById('btn5').style.display = 'block'
+}
+function btn5(){
+    document.getElementById('dis5').style.display = 'block'
+}
+
+function pXato(){
+    let pXato = document.getElementById("pXato").value;
+    let pXato1 = document.getElementById("pXato1").value;
+    let pXato2 = document.getElementById("pXato2").value;
+    let pTugri = document.getElementById("pTugri").value;
+    let pTugri1 = document.getElementById("pTugri1").value;
+    let pTugri2 = document.getElementById("pTugri2").value;
+    let pXatolar = pXato + pXato1 + pXato2;
+    let pTugrilar = pTugri + pTugri1 + pTugri2;
+    if(pXatolar != pTugrilar){
+        document.getElementById("smsXato").style.display = "block";
+        document.getElementById("pQayta").style.visibility = "visible";
+    }
+    if(pXatolar == pTugrilar){
+        document.getElementById("smsTugri").style.display = "block";
+        document.getElementById("qadam_2").style.display = "block";
+    }
+}
+function pNone() {
+    document.getElementById('smsXato').style.display = "none";
+}
