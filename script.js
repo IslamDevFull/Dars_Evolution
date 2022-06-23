739f3cc042226abcb420c5bff51bc5402a2cc3d0
IslamDevFull <it.front.back@gmail.com>
2022-06-23 17:10:16 +0500

    mavzu 1 va 2 tax bo'ldi

--- script.js
+++ script.js
@@ -22,27 +22,36 @@ function none() {
 }
 
 function btn(){
-    document.getElementById('dis').style.display = 'block'
-    document.getElementById('btn1').style.display = 'block'
+    document.getElementById('dis').style.display = 'block';
+    document.getElementById('btn1').style.display = 'block';
 }
 function btn1(){
-    document.getElementById('dis1').style.display = 'block'
-    document.getElementById('btn2').style.display = 'block'
+    document.getElementById('dis1').style.display = 'block';
+    document.getElementById('btn2').style.display = 'block';
 }
 function btn2(){
-    document.getElementById('dis2').style.display = 'block'
-    document.getElementById('btn3').style.display = 'block'
+    document.getElementById('dis2').style.display = 'block';
+    document.getElementById('btn3').style.display = 'block';
 }
 function btn3(){
-    document.getElementById('dis3').style.display = 'block'
-    document.getElementById('btn4').style.display = 'block'
+    document.getElementById('dis3').style.display = 'block';
+    document.getElementById('btn4').style.display = 'block';
 }
 function btn4(){
-    document.getElementById('dis4').style.display = 'block'
-    document.getElementById('btn5').style.display = 'block'
+    document.getElementById('dis4').style.display = 'block';
+    document.getElementById('btn5').style.display = 'block';
 }
 function btn5(){
-    document.getElementById('dis5').style.display = 'block'
+    document.getElementById('dis5').style.display = 'block';
+    document.getElementById('btn6').style.display = 'block';
+}
+function btn6(){
+    document.getElementById('dis6').style.display = 'block';
+    document.getElementById('btn7').style.display = 'block';
+}
+function btn7(){
+    document.getElementById('dis7').style.display = 'block';
+    document.getElementById('davom').style.display = 'block';
 }
 
 function pXato(){
@@ -60,9 +69,28 @@ function pXato(){
     }
     if(pXatolar == pTugrilar){
         document.getElementById("smsTugri").style.display = "block";
-        document.getElementById("qadam_2").style.display = "block";
+        document.getElementById("p2").style.display = "block";
     }
 }
 function pNone() {
+    document.getElementById('smsXato').style.display = "none";
+}
+function bXato(){
+    let bXato = document.getElementById("bXato").value;
+    let bXato2 = document.getElementById("bXato2").value;
+    let bTugri = document.getElementById("bTugri").value;
+    let bTugri2 = document.getElementById("bTugri2").value;
+    let bXatolar = bXato + bXato2;
+    let bTugrilar = bTugri + bTugri2;
+    if(bXatolar != bTugrilar){
+        document.getElementById("smsXato").style.display = "block";
+        document.getElementById("bQayta").style.visibility = "visible";
+    }
+    if(bXatolar == bTugrilar){
+        document.getElementById("smsTugri").style.display = "block";
+        document.getElementById("b3").style.display = "block";
+    }
+}
+function bNone() {
     document.getElementById('smsXato').style.display = "none";
 }
