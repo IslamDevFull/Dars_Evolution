c6aca8117963cbc1d388aae66492542c09b24a1d
IslamDevFull <it.front.back@gmail.com>
2022-07-04 12:34:29 +0200

    Mavzu 1 da img bosilaydigan qilinobti

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
 function xato(){
     let xato = document.getElementById("xato").value;
     let xato1 = document.getElementById("xato1").value;
-    let xato2 = document.getElementById("xato2").value;
     let tugri = document.getElementById("tugri").value;
     let tugri1 = document.getElementById("tugri1").value;
-    let tugri2 = document.getElementById("tugri2").value;
-    let xatolar = xato + xato1 + xato2;
-    let tugrilar = tugri + tugri1 + tugri2;
+    let xatolar = xato + xato1;
+    let tugrilar = tugri + tugri1;
     if(xatolar != tugrilar){
         document.getElementById("smsXato").style.display = "block";
         document.getElementById("qayta").style.visibility = "visible";
